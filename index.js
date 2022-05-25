b966c5f63f2afbfcc37199a1a0e33466fa594af0
roshnirifa <roshnirifa998@gmail.com>
2022-05-25 13:34:47 +0600

    all tools added

--- index.js
+++ index.js
@@ -8,6 +8,43 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1l4vypy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connect');
+
+        const allToolsCollection = client.db("machanic-shop").collection("allTools");
+
+        // all products POST
+        app.get('/tools', async (req, res) => {
+
+            const query = {};
+            const cursor = allToolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.json(tools)
+
+        })
+        app.get('/tools/:id', async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: ObjectId(id) };
+            const result = await allToolsCollection.findOne(query);
+            res.json(result)
+
+        })
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('machanic shop')
 })
